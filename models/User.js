0dc7621b6c64d76f545f703a3093b1bb804bbdc6
deadlock314 <chandansoni2270@gmail.com>
2022-12-02 18:29:52 +0530

    added address , imgLinks and status in user schema

--- models/User.js
+++ models/User.js
@@ -1,39 +1,76 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 
-const UserSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        minlength:3,
-        maxlength:256
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 256
     },
-    metaMaskAddress:{
-        type:String,
-        maxlength:256
-    },
-    maxOutput:{
-        type:Number,
-    },
-    availableOutput:{
-        type:Number,
-    },
-    contact:{
-        type:Number,
-    },
-    buyFrom:{
-        type:[String],
 
+    InfraImgLink: {
+        type: String
+    }
+    ,
+    DPImgLink: {
+        type: String
     },
-    sellTo:{
-        type:[String],
 
+    active: {
+        type: Boolean,
+        default: true
     },
     
+    rentedcable: {
+        type: Boolean,
+        default: true
+    },
+
+    Address: {
+        type: Object,
+
+        state: {
+            type: String
+        },
+        pincode: {
+            type: String
+        },
+        city: {
+            type: String,
+        },
+        landmark: {
+            type: String,
+        },
+
+    },
+
+    metaMaskAddress: {
+        type: String,
+        maxlength: 256
+    },
+    maxOutput: {
+        type: Number,
+    },
+    availableOutput: {
+        type: Number,
+    },
+    contact: {
+        type: Number,
+    },
+    buyFrom: {
+        type: [String],
+
+    },
+    sellTo: {
+        type: [String],
+
+    },
+
 
 
 
 });
 
-const User = mongoose.model('User',UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
