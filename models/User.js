4fc34e41ae6d3e16d5590d5bbb140d918cb0f48c
Devesh Swarnkar <deveshswarnkar007@gmail.com>
2022-12-05 18:26:24 +0530

    added some in User model

--- models/User.js
+++ models/User.js
@@ -1,39 +1,64 @@
 const mongoose= require('mongoose');
 
-const UserSchema=mongoose.Schema({
+const userSchema=mongoose.Schema({
     name:{
         type:String,
         required:true,
         minlength:3,
         maxlength:256
     },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+        maxlength:256,
+        minlength:10
+    },
+    password:{
+      type:String,
+      required:true
+    },
+    verifiedMail:{
+        type:Boolean,
+        default:false
+    },
     metaMaskAddress:{
         type:String,
-        maxlength:256
+        maxlength:256,
+        unique:true,
+        sparse:true
     },
-    maxOutput: {
-        type: Number,
+    isNode:{
+        type:Boolean,
+        default:false
     },
-    availableOutput: {
-        type: Number,
+    maxOutput:{
+        type:Number,
     },
-    contact: {
-        type: Number,
+    availableOutput:{
+        type:Number,
+    },
+    contact:{
+        type:Number,
+    },
+    verifiedContact:{
+        type:Boolean,
+        default:false
     },
     buyFrom:{
         type:[String],
 
     },
-    sellTo: {
-        type: [String],
+    sellTo:{
+        type:[String],
 
     },
-
+    
 
 
 
 });
 
-const User = mongoose.model('User',UserSchema);
+const User = mongoose.model('User',userSchema);
 
 module.exports = User;
